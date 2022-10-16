142b48ce2016e6581a0ccc78224f33d674b2a6e9
tahlil <bsse0803@iit.du.ac.bd>
2022-10-16 16:10:46 +0600

    Load ngrok

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const ngrok = require('ngrok');
+
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(3000, () => console.log("Server listening on port 3000!"));
+app.listen(80, () => console.log("Server listening on port 3000!"));
+
+(async function() {
+    const url = await ngrok.connect();
+    console.log(url);
+  })();
