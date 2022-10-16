f29daebf7e78be0e888c9ad6a3353ab671c5ff4c
tahlil <bsse0803@iit.du.ac.bd>
2022-10-16 16:20:03 +0600

    Load ngrok authtoken

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const ngrok = require('ngrok');
+require('dotenv').config();
 
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(80, () => console.log("Server listening on port 3000!"));
-
+app.listen(80, () => console.log("Server listening on port 80!"));
 (async function() {
-    const url = await ngrok.connect();
-    console.log(url);
+    try {
+        const url = await ngrok.connect({authtoken: process.env.NGROK_AUTH_TOKEN});
+        console.log(url);
+    } catch (error) {
+        console.log(error);
+    }
+    
   })();
